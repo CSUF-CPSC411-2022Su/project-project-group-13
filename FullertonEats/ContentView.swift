67a6ea08d1d8d483f107e06e40b7026c43e526d8
ericchu1329 <Kiyuen88ec@gmail.com>
2022-06-08 14:28:33 -0700

    Initial Commit

---
+++ FullertonEats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FullertonEats
+//
+//  Created by Eric Chu on 6/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FullertonEats/FullertonEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullertonEatsApp.swift
+//  FullertonEats
+//
+//  Created by Eric Chu on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FullertonEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
