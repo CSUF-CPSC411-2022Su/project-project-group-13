c976d5f6ea14f03f58b7fb7c1854d9983b6a0038
ericchu1329 <Kiyuen88ec@gmail.com>
2022-06-12 21:43:36 -0700

    created services screen

--- FullertonEats/ContentView.swift
+++ FullertonEats/ContentView.swift
@@ -9,8 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Button("Add", action: {
+                    print("adding")
+                }).foregroundColor(.orange)
+                Spacer().frame(width: 270)
+                Button("Edit", action: {
+                    print("editing")
+                }).foregroundColor(.orange)
+            }
+            HStack{
+                Text("My Services")
+                    .fontWeight(.bold)
+                    .frame(width: 330, height: 25, alignment: .topLeading)
+            }
+            List(0..<5) { item in
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Label")
+                            .fontWeight(.semibold)
+                            .minimumScaleFactor(0.5)
+                        
+                        Text("January 1, 2021")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                        
+                        Text("6:00pm - 8:00pm")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                    Spacer().frame(width: 160)
+                    Image(systemName: "info.circle")
+                }
+            }
+        }
     }
 }
 
--- FullertonEats/Services.swift
+++ FullertonEats/Services.swift
@@ -7,9 +7,41 @@
 
 import SwiftUI
 
-struct Services {
+struct Service {
+    var label: String
+    var desc: String
+    var address: String
+    var date: DateComponents
+    var time: DateComponents
     
+    // with description
+    init(label: String, desc: String, address: String, date: DateComponents, time: DateComponents) {
+        self.label = label
+        self.desc = desc
+        self.address = address
+        self.date = date
+        self.time = time
+    }
     
+    // no description
+    init(label: String, address: String, date: DateComponents, time: DateComponents) {
+        self.label = label
+        self.desc = ""
+        self.address = address
+        self.date = date
+        self.time = time
+    }
+
+}
+
+class Services {
     
+    var myServices: [Service] = []
+    
+    init() {}
+    
+    func addService(service: Service) {
+        myServices.append(service)
+    }
     
 }
