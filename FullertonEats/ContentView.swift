4f63de31ba58169445b2dcf6a559309ffde6e600
aellux <alexluvg13@gmail.com>
2022-06-14 18:48:15 -0700

    created sign up page with textfields, background, and button for signup

--- FullertonEats/ContentView.swift
+++ FullertonEats/ContentView.swift
@@ -8,9 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usernameS = ""
+    @State private var passwordS = ""
+    @State private var emailS = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ZStack{
+                Color.blue
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white)
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.orange)
+                
+                VStack{
+                    Text("FullertonEats")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    Text("Sign Up")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $usernameS)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                    TextField("Password", text: $passwordS)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                    TextField("Email", text: $emailS)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                    Button("signup"){
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                }
+            }
+            .navigationBarHidden(true)
+        }
     }
 }
 
