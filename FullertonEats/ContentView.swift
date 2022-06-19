aaa12d79f9e38bfdbc48ad9da2032a73feede965
Freddy Nunez <fnunez004@csu.fullerton.edu>
2022-06-18 19:24:58 -0700

    Moved Login Screen view feature from ContentView.swift to LoginScreenView.swift

--- FullertonEats/ContentView.swift
+++ FullertonEats/ContentView.swift
@@ -8,51 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var username = ""
-    @State private var password = ""
-    
-    var body: some View {
-        NavigationView{
-            ZStack{
-                Color.blue
-                    .ignoresSafeArea()
-                Circle()
-                    .scale(1.7)
-                    .foregroundColor(.white)
-                Circle()
-                    .scale(1.35)
-                    .foregroundColor(.orange)
-                
-                VStack{
-                    Text("FullertonEats")
-                        .font(.largeTitle)
-                        .bold()
-                        .padding()
-                    Text("login")
-                        .font(.largeTitle)
-                        .bold()
-                        .padding()
-                    TextField("Username", text: $username)
-                        .padding()
-                        .frame(width: 300, height: 50)
-                        .background(Color.white)
-                        .cornerRadius(10)
-                    TextField("Password", text: $password)
-                        .padding()
-                        .frame(width: 300, height: 50)
-                        .background(Color.white)
-                        .cornerRadius(10)
-                    Button("login"){
-                        //authenticate user if they have an account set up
-                    }
-                    .foregroundColor(.white)
-                    .frame(width: 300, height: 50)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                }
-            }
-            .navigationBarHidden(true)
-        }
+    var body: some View{
+        Text("fullerton")
     }
 }
 
