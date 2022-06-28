eb144f06de83c608d03b029bf8af7eb774c7dd3b
Jiu Lin <linj_happy@csu.fullerton.edu>
2022-06-27 23:44:31 -0700

    Final_Projec_Done

--- FullertonEats/Modifiers.swift
+++ FullertonEats/Modifiers.swift
@@ -32,3 +32,29 @@ struct TitleModifier: ViewModifier {
     }
 }
 
+
+//Jiu Lin' struct
+struct TextEntry: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(10)
+            .border(Color.blue)
+            .background(Color.white)
+            .foregroundColor(Color.black)
+            .cornerRadius(8)
+    }
+}
+
+struct myButtonDesign: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .imageScale(.large)
+            .padding()
+            .font(.headline)
+            // .background(Color(red: 0.8823529411764706, green: 0.4392156862745098, blue: 0.0))
+            .background(Color.blue)
+            .foregroundColor(Color.orange)
+            .multilineTextAlignment(.leading)
+            .cornerRadius(10)
+    }
+}
